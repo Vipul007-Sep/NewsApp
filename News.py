4626fd4540f8df9f14e83d7654f1498487c2da89
vipul007-Sep <vipuldixit0007@gmail.com>
2025-06-05 19:20:51 +0530

    Keyword logic updated to take multiple keywords

--- News.py
+++ News.py
@@ -15,7 +15,7 @@ def get_news(api_key, keyword, language='en', page_size=100):
         'pageSize': page_size,
         'apiKey': api_key
     }
-
+    
     response = requests.get(url, params=parameters)
 
     if response.status_code == 200:
@@ -25,13 +25,11 @@ def get_news(api_key, keyword, language='en', page_size=100):
             print("No articles found for the keyword.")
             return
         
-        new_articles_found = False
         i = 1
         for article in articles:
             article_id = article['url']
             if article_id not in seen_articles:
                 seen_articles.add(article_id)
-                new_articles_found = True
                 print(f"{i}. Title: {article['title']}")
                 print(f"   Published At: {article['publishedAt']}")
                 print(f"   Author: {article['author']}")
@@ -39,17 +37,23 @@ def get_news(api_key, keyword, language='en', page_size=100):
                 print(f"   URL: {article['url']}\n")
                 i += 1
 
-        if not new_articles_found:
-            print("No new articles found.")
     else:
         print(f"Failed to fetch news. Status Code: {response.status_code}")
         print(response.json())
 
 if __name__ == '__main__':
     api_key = os.getenv('NEWS_API_KEY')
-    keyword = input("Enter the keyword to search news for: ")
+    keywords_str = os.getenv('NEWS_KEYWORDS')
+
+    if not keywords_str:
+        print("NEWS_KEYWORDS environment variable is not set.")
+        exit(1)
+
+    keywords = [k.strip() for k in keywords_str.split(',') if k.strip()]
+
     
     while True:
-        get_news(api_key, keyword)
+        for keyword in keywords:
+            get_news(api_key, keyword)
         print("Waiting for the next check...\n")
-        time.sleep(60)  
+        time.sleep(60) 
